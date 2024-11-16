0ec68379e4aff29bf93ab338cf0c1be26da2eec3
icebear-yy <20242462@stu.cqu.edu.cn>
2024-11-16 16:47:42 +0800

    Add files via upload

---
+++ exercise1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string name ;
+    cout<<"姓名";
+    getline(cin,name);
+    cout<<"hello "<<name;
+    return 0;
+
+}
---
+++ exercise1.py
@@ -0,0 +1,2 @@
+name = input("请输入你的名称: ")
+print(f"hello {name}!")
